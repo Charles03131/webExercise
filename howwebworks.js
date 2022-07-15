feaa92689f6bcae0fa0e38f01b461f4c5d3d04de
Charles03131 <97250801+Charles03131@users.noreply.github.com>
2022-07-14 23:27:07 -0500

    Add files via upload

---
+++ howwebworks.js
@@ -0,0 +1,23 @@
+
+
+
+1.  HTTP is Hypertext transfer protocol and is used to communicate between browsers and web servers.
+2. URL is Uniform resource locator. URL is the address to a published resource on the web.
+3. The DNS (domain name system) translates domain names into IP addresses so that browsers can load sources. 
+   This eliminates the need to remember lengthy IP numbers. It is much easier to remember that way.
+4.   A query string is the portion of a URL where data is passed to web applications/back-end.
+5.GET -retrieves or reads information from a source as a representation  in the form of XML or JSON. 
+ POST- is used to create new resources that are subordinate to a parent resource.
+
+6.An HTTP request is a request made by a client to a named host that is located on a server.
+
+7. An HTTP response is carried out by the server to the client to provide the requested information. 
+8. Headers provide additional information about the request or response. 
+      Some examples of request headers are accept-language, accept-encoding.
+      Some examples of response headers are content-encoding, content-type.
+9.   The browser uses DNS to make an IP out of the URL, requests are sent to the IP 
+       The server makes a response, the browser makes a DOM out of the response, lastly the browser makes new
+        requests for any new information needed from the DOM. 
+
+
+
